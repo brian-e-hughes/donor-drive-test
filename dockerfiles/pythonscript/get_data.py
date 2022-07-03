ede1ea681a08fdf1413d59ace0bd61974469b839
Brian Hughes <hughesbrian@protonmail.com>
2022-07-02 21:40:18 -0400

    removing sleep statement from python script

--- dockerfiles/pythonscript/get_data.py
+++ dockerfiles/pythonscript/get_data.py
@@ -5,8 +5,6 @@ from elasticsearch import Elasticsearch
 extra_life_url = "https://www.extra-life.org/api/participants/477670/donations"
 donations = requests.get(extra_life_url)
 
-sleep(60)
-
 es = Elasticsearch("http://elasticsearch:9200")
 response = es.indices.create(index="donations")
 print(response)
